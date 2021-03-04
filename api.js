5982848ff2db1b9497cf338ecd6bc507d984c6f7
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-03-04 17:20:34 +0200

    response format tests

--- api.js
+++ api.js
@@ -1,11 +1,14 @@
+"use strict"
 const express = require("express");
 const api = express.Router();
-
+const DOMAIN = "http://localhost:3000";
 api.use(express.json());
 
 api.post("/shorturl/new", (req, res) => {
-    console.log(req.body);
-    res.send("k");
+    const original_url = req.body.url;
+    const short_url = DOMAIN + 2;
+    const response = {original_url, short_url};
+    res.send(response);
 });
 
 
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,11 +1,26 @@
+const { response } = require('express');
 const request = require('supertest');
 const app = require("../app");
 
-test("mock test", () => {
+test("Always PASS", () => {
     expect(true).toBe(true);
 });
 
 //backend service tests
-test("POST api/shorturl/new", async () => {
-    const response = await request(app).post("api/shorturl/new").send({});
-}); 
+describe("URL shortener API", () => {
+    const mockURL = "http://google.com";
+    const resPromise = request(app)
+    .post("/api/shorturl/new")
+    .send({url: mockURL})
+    .set("Content-Type", "application/json");
+    test("response body has 'original_url' property", async () => {
+        const {body} = await resPromise;
+        expect(body.hasOwnProperty("original_url")).toBe(true);
+    }); 
+    test("response body has 'short_url' property", async () => {
+        const {body} = await resPromise;
+        expect(body.hasOwnProperty("short_url")).toBe(true);
+    }); 
+    
+
+});
