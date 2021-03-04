a819b19b9256d652dd657ad4729ba52a7ad442fa
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-03-04 17:33:05 +0200

    organize tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -8,18 +8,22 @@ test("Always PASS", () => {
 
 //backend service tests
 describe("URL shortener API", () => {
-    const mockURL = "http://google.com";
-    const resPromise = request(app)
-    .post("/api/shorturl/new")
-    .send({url: mockURL})
-    .set("Content-Type", "application/json");
-    test("response body has 'original_url' property", async () => {
-        const {body} = await resPromise;
-        expect(body.hasOwnProperty("original_url")).toBe(true);
-    }); 
-    test("response body has 'short_url' property", async () => {
-        const {body} = await resPromise;
-        expect(body.hasOwnProperty("short_url")).toBe(true);
+
+    describe("response format" , () => {
+        const mockURL = "http://google.com";
+        const resPromise = request(app)
+        .post("/api/shorturl/new")
+        .send({url: mockURL})
+        .set("Content-Type", "application/json");
+        
+        test("response body has 'original_url' property", async () => {
+            const {body} = await resPromise;
+            expect(body.hasOwnProperty("original_url")).toBe(true);
+        }); 
+        test("response body has 'short_url' property", async () => {
+            const {body} = await resPromise;
+            expect(body.hasOwnProperty("short_url")).toBe(true);
+        });
     }); 
     
 
