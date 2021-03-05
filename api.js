a8fed587411a3f11f43a48ddd7ed2d1beb06a1bc
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-03-05 18:44:11 +0200

    create/get short url end point

--- api.js
+++ api.js
@@ -1,15 +1,37 @@
 "use strict"
 const express = require("express");
+const DataBase = require("./DB")
 const api = express.Router();
 const DOMAIN = process.env.HOST_NAME || `http://localhost:${process.env.PORT}`;
+
+const dataBase = new DataBase();
 
 api.use(express.json());
 
 api.post("/shorturl/new", (req, res) => {
+    if( !req.body.hasOwnProperty("url") ) return res.status(400).send({message: "request body must have 'url' property"});
     const original_url = req.body.url;
-    const short_url = 13;//DOMAIN + "/1";
-    const response = {original_url, short_url};
-    res.send(response);
+    try {
+        new URL(original_url);
+    } catch (e) {
+        console.log(e);
+        return res.status(400).send({ message: "url must be valid"});
+    }
+
+    dataBase.getItemByProperty("original_url", original_url)
+    .then(shortUrlBin => {
+        const id = shortUrlBin["shorturl-id"];
+        const short_url = `${DOMAIN}/${id}`;
+        res.status(200).send({original_url, short_url})
+    })
+    .catch(() => {
+        const newShortUrlBin = createShortURL(original_url);
+        const id = newShortUrlBin["shorturl-id"];
+        const short_url = `${DOMAIN}/${id}`;
+        return dataBase.setItem(id, newShortUrlBin)
+    })
+    .then( () => res.status(201).send(original_url, short_url) )
+    .catch( error => res.status(500).send(error) );
 });
 
 
