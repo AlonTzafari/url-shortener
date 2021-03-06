7a1c58960b2c7812a44c5ec773a5fd9c9d374622
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-03-06 14:43:04 +0200

    page renders short url after posting

--- public/script.js
+++ public/script.js
@@ -7,5 +7,24 @@ async function onLoad() {
         event.preventDefault();
         const url = form.querySelector("#url_input").value;
         const res = await axios.post( "http://localhost:3000/api/shorturl/new", {url} );
+        const shortUrl = res.data.short_url;
+        const outputDiv = document.querySelector(".output-container");
+        outputDiv.innerHTML = "";
+        const shortUrlElement = document.createElement("a");
+        shortUrlElement.innerText = shortUrl;
+        shortUrlElement.href = shortUrl;
+        shortUrlElement.id = "url-output";
+
+        const copyBtn = document.createElement("button");
+        copyBtn.innerText = "COPY";
+        copyBtn.classList.add("copy-button");
+
+        outputDiv.append(shortUrlElement, copyBtn);
     });
 }
+
+
+{/* <div class="output-container">
+<a id = "url-output" href="">shorturl.com</a>
+<button>COPY</button>
+</div> */}
